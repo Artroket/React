bd804b1a06c360cb483965e67b82dee46818b235
Artem <artemtarasenko997@gmail.com>
2020-08-13 22:45:20 +0300

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,49 @@
 import React from 'react';
 import './App.css';
 
-const App = () =>{
-  return (
-    <div>
-    <Header />
-    <Technologies />
-    </div>
-  );
-}
 
-const Header = () =>{
-  return (
-    <div>
-    <a href ='#s'>Home</a>
-    <a href ='#s'>Feed</a> 
-    <a href ='#s'>Messages</a>
-    </div>
-  );
-}
 
-const Technologies = () =>{
-  return(
-    <div>
-      <ul>
-        <li>Html</li>
-        <li>Css</li>
-        <li>Js</li>
-        <li>React</li>
-      </ul>
+
+const App = () => {
+  return (
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png' />
+      </header>
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+      </nav>
+      <div className='content'>
+        <div>
+          <img src='https://cdn3.photoblogstop.com/wp-content/uploads/2012/07/Sierra_HDR_DFX8048_2280x819_Q40_wm_mini-1726x819__.jpg' />
+        </div>
+        <div>
+          <img src='https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png' />
+        </div>
+        <div>
+          My posts
+          <div>
+            New Posts
+          </div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post2
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
