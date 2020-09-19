e33a86f8346eea5e83f9a2285a71efd79098d5e1
Artem <artemtarasenko997@gmail.com>
2020-09-19 20:00:33 +0300

    Pagination

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,12 +1,15 @@
 import React, { useImperativeHandle } from 'react';
 import Users from './Users';
-import { setUsersAC, followAC, unfollowAC} from '../../redux/users-reducer';
+import { setUsersAC, followAC, unfollowAC, setCurrentPageAC, setTotalUsersCountAC} from '../../redux/users-reducer';
 import { connect } from 'react-redux';
  
 
 let mapStateToProps = (state) =>{
     return {
-        users:  state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
     }
 }
 
@@ -20,6 +23,12 @@ let mapDispatchToProps = (dispatch) =>{
         },
         setUsers: (users) =>{
             dispatch(setUsersAC(users))
+        },
+        setCurrentPage: (pageNumber) => {
+            dispatch(setCurrentPageAC(pageNumber))
+        },
+        setTotalUsersCount: (totalCount) =>{
+            dispatch(setTotalUsersCountAC(totalCount))
         }
     }
 }
